d1dbfe19d93e04594797ddfda2b1273c606244e1
tmddus081 <127263700+tmddus081@users.noreply.github.com>
2023-03-15 10:46:23 +0900

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtname_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclick_Click(object sender, EventArgs e)
+        {
+           if (txtname.Text != "")
+            lblhello.Text = txtname.Text + "님! 안녕하세요";
+            else
+                MessageBox.Show("이름을 입력하세요", "Warning");
+                
+        }
+    }
+}
