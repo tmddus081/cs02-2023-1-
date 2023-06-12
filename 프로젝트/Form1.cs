20f87c4fd7674d3f072ea46befe9ec0835bf109d
tmddus081 <127263700+tmddus081@users.noreply.github.com>
2023-06-12 18:18:57 +0900

    Add files via upload

---
+++ 프로젝트/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 프로젝트
+{
+    public partial class Form1 : Form
+    {
+        enum Operators
+        {
+            None, add, subtract, Multiply, Divide, Result
+        }
+
+        Operators currentOperator = Operators.None;
+        Boolean operatorChangeFlag = false;
+        int firstOperand = 0;
+        int secondOperand = 0;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonSeven_Click(object sender, EventArgs e)
+        {
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+
+            string strNumber = display.Text += "7";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void Buttoneight_Click(object sender, EventArgs e)
+        {
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+
+            string strNumber = display.Text += "8";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void ButtonNine_Click(object sender, EventArgs e)
+        {
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+
+            string strNumber = display.Text += "9";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void ButtonDivide_Click(object sender, EventArgs e)
+        {
+            firstOperand = Int32.Parse(display.Text);
+            currentOperator = Operators.Divide;
+            operatorChangeFlag = true;
+        }
+
+        private void ButtonFour_Click(object sender, EventArgs e)
+        {
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+
+            string strNumber = display.Text += "4";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void ButtonFive_Click(object sender, EventArgs e)
+        {
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+
+            string strNumber = display.Text += "5";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void ButtonSix_Click(object sender, EventArgs e)
+        {
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+
+            string strNumber = display.Text += "6";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void ButtonMultiply_Click(object sender, EventArgs e)
+        {
+            firstOperand = Int32.Parse(display.Text);
+            currentOperator = Operators.Multiply;
+            operatorChangeFlag = true;
+        }
+
+        private void ButtonOne_Click(object sender, EventArgs e)
+        {
+           if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+            
+            string strNumber = display.Text += "1";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void ButtonTwo_Click(object sender, EventArgs e)
+        {
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+
+            string strNumber = display.Text += "2";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void ButtonThree_Click(object sender, EventArgs e)
+        {
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+
+            string strNumber = display.Text += "3";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void ButtonSubtract_Click(object sender, EventArgs e)
+        {
+            firstOperand = Int32.Parse(display.Text);
+            currentOperator = Operators.subtract;
+            operatorChangeFlag = true;
+        }
+
+        private void ButtonAllClear_Click(object sender, EventArgs e)
+        {
+            firstOperand = 0;
+            secondOperand = 0;
+            currentOperator = Operators.None;
+            display.Text = "0";
+        }
+
+        private void ButtonZero_Click(object sender, EventArgs e)
+        {
+            if (operatorChangeFlag == true)
+            {
+                display.Text = "";
+                operatorChangeFlag = false;
+            }
+            string strNumber = display.Text += "0";
+            int intNumber = Int32.Parse(strNumber);
+            display.Text = intNumber.ToString();
+        }
+
+        private void ButtonPoint_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            firstOperand = Int32.Parse(display.Text);
+            currentOperator = Operators.add;
+            operatorChangeFlag = true;
+        }
+
+        private void ButtonResult_Click(object sender, EventArgs e)
+        {
+            secondOperand = Int32.Parse(display.Text);
+            if(currentOperator == Operators.add)
+            {
+                firstOperand += secondOperand;
+                display.Text = firstOperand.ToString();
+            }
+
+            else if(currentOperator == Operators.subtract)
+            {
+                firstOperand -= secondOperand;
+                display.Text = firstOperand.ToString();
+            }
+
+            else if (currentOperator == Operators.Multiply)
+            {
+                firstOperand *= secondOperand;
+                display.Text = firstOperand.ToString();
+            }
+
+            else if (currentOperator == Operators.Divide)
+            {
+                if(secondOperand == 0)
+                {
+                    display.Text = "0으로 나눌 수 없습니다";
+                }
+                else
+                {
+                    firstOperand /= secondOperand;
+                    display.Text = firstOperand.ToString();
+                }
+            }
+
+        }
+    }
+}
